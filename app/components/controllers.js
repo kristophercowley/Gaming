8c6462a0d46510d0787cc37213142017d3a304db
Kristopher Cowley <kristopherleecowley@gmail.com>
2016-05-06 12:59:24 -0600

    focus()

--- app/components/controllers.js
+++ app/components/controllers.js
@@ -3,22 +3,48 @@ angular.module('game-app')
         var gc = this;
         gc.test = "test";
         var footElem = document.getElementById('foot');
+        footElem.innerHTML = '<img src="images/footdark.png" alt="Bigfoot">';
 
+
+
+
+        // sdfghjkjhgfd
         gc.moveByKey = function (e) {
             console.log(e)
-            console.log('Function works')
+            var direction = {
+                37: 'left',
+                38: 'up',
+                39: 'right',
+                40: 'down',
+                32: 'spacebar',
+                13: 'enter'
 
+            }
             if (e.keyCode == '38') {
                 console.log('up');
-                footElem
+                footElem.style.top ++;
             } else if (e.keyCode == '40') {
+                footElem.style.top ++;
                 console.log('down');
+            } else if (e.keyCode == '39') {
+                console.log('right')
+                footElem.style.left  ++;
 
+            } else if (e.keyCode == '37') {
+                console.log('left')
+                footElem.style.left  ++;
 
-                // keyPress = -1;
             } else if (e.keyCode == '13') {
-                cl.saveToHistory(cl.command);
-                cl.command = "";
+                console.log("enter")
+                footElem.style.right ++;
+
+            } else if (e.keyCode == '32') {
+                console.log('spacebar')
+                footElem.style.top  ++;
+
             }
         }
+
+        document.getElementById('contain').focus();
+
     })
